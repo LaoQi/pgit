3cdf0e64252895182f074f5cb978a47759f107e2
LaoQi <qjqjqzy@gmail.com>
2019-03-28 00:34:56 +0800

    readme

--- Readme.md
+++ Readme.md
@@ -1,3 +1,11 @@
 # pgit
 
 personal git server
+
+## roadmap
+
+* http api
+* embed ssh
+* webUI
+* search
+* mirror hook webhook
--- repo.go
+++ repo.go
@@ -151,5 +151,6 @@ func (repoApi RepoApi) View(w http.ResponseWriter, r *http.Request) {
 		w.Write([]byte(err.Error()))
 		return
 	}
+	w.Header().Set("Content-Type", "application/json; charset=utf-8")
 	w.Write(output)
 }
